b8662017c95db2f890a1de4616dde91bee3473c2
Rair Suzuki <rairsuzukipr@gmail.com>
2024-12-07 17:47:35 -0300

    fix image routes import

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from app.api import sessions, auth
+from app.api import sessions, auth, images
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(sessions.router, prefix="/sessions", tags=["sessions"])
+app.include_router(images.router, prefix="/images", tags=["sessions"])
 
 @app.get("/health-check")
 def health_check():
