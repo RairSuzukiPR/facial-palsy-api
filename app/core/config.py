9d03f19844fcd729bbb09f3751c9b333eff4d08b
Rair Suzuki <rairsuzukipr@gmail.com>
2024-12-08 10:46:48 -0300

    fix expiration token time

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
 
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
