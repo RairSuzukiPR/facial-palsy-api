3bb51835c0229331a5e89759720910a78a99d420
Rair Suzuki <rairsuzukipr@gmail.com>
2024-12-21 17:27:43 -0300

    sunnybrook rest symmetry score implemented

--- app/services/sessions_service.py
+++ app/services/sessions_service.py
@@ -11,11 +11,16 @@ from app.services.images_service import get_face_landmarks_detection
 class SessionService:
     def __init__(self, db_connection: mysql.connector.MySQLConnection):
         self.connection = db_connection
+        self.paralyzed_side = None
+
         self.right_eyebrow_pts = [70, 63, 105, 66, 107, 46, 53, 52, 65, 55]
         self.left_eyebrow_pts = [300, 293, 334, 296, 336, 276, 283, 282, 295, 285]
+        self.average_line_pt = 168
         self.right_mouth_end_pt = 61
         self.left_mouth_end_pt = 291
-        self.paralyzed_side = None
+        self.right_eye_open_pts = [159, 145]
+        self.left_eye_open_pts = [386, 374]
+
 
     def new_session(self, user_id: int):
         cursor = self.connection.cursor()
@@ -52,11 +57,12 @@ class SessionService:
         results_by_expression = self._process_images(images)
 
         house_brackmann_score = self.get_house_brackmann_classif(results_by_expression)
+        sunnybrook_score = self.get_sunnybrook_classif(results_by_expression)
 
         return SessionResult(
             session_id=session_id,
             house_brackmann=house_brackmann_score,
-            sunnybrook=20,
+            sunnybrook=sunnybrook_score,
             photos=['TBD'],
             photos_with_poitns=['TBD'],
         )
@@ -156,6 +162,53 @@ class SessionService:
         else:
             return "Grau I (Normal)"
 
+    def get_sunnybrook_classif(self, results_by_expression):
+        rest_symmetry_score = self.calculate_SB_rest_symmetry_score(results_by_expression)
+        movement_symmetry_score = self.calculate_SB_movement_symmetry_score(results_by_expression)
+        synkinesis_score = self.calculate_SB_synkinesis_score(results_by_expression)
+
+        # ter um de/para aq?
+        return rest_symmetry_score + movement_symmetry_score + synkinesis_score
+
+    def calculate_SB_rest_symmetry_score(self, results_by_expression):
+        # TODO: verificar se cirurgia de palpebra
+        distances_eyes = self._calculate_distance_from_expression_pts(
+            results_by_expression,
+            ['Repouso'],
+            self.left_eye_open_pts,
+            self.right_eye_open_pts
+        )
+        paralyzed_side_distance_eyes = distances_eyes[0 if self.paralyzed_side == 'left' else 1]
+        normal_side_distance_eyes = distances_eyes[1 if self.paralyzed_side == 'left' else 0]
+        perc_variation_eyes = abs(normal_side_distance_eyes - paralyzed_side_distance_eyes) / normal_side_distance_eyes * 100
+        eye_score = 1 if perc_variation_eyes > 20 else 0
+        # print('perc_variation_eyes', distances_eyes, perc_variation_eyes, eye_score)
+
+        # TODO:  verificar nas perguntas: Voce tem bigode chines?
+        # -> sim, Voce tem bigode chines apenas no lado paralisado? (sim=2, nao=1);
+        # -> nao, 0
+        cheeks_score = 0
+
+        distances_mouth = self._calculate_distance_mouth_variation(
+            results_by_expression,
+            ['Repouso'],
+            self.left_mouth_end_pt,
+            self.right_mouth_end_pt,
+            self.average_line_pt
+        )
+
+        perc_variation_mouth = abs(distances_mouth[0] - distances_mouth[1]) / distances_mouth[0] * 100
+        mouth_score = 1 if perc_variation_mouth > 20 else 0
+        # print('perc_variation_mouth', distances_mouth, perc_variation_mouth, mouth_score)
+
+        return (eye_score + cheeks_score + mouth_score) * 5
+
+    def calculate_SB_movement_symmetry_score(self, results_by_expression):
+        return 0
+
+    def calculate_SB_synkinesis_score(self, results_by_expression):
+        return 0
+
     def _calculate_distance_between_expression_pts(self, results_by_expression, expressions, left_pt, right_pt):
         results = []
         points_by_expression = []
@@ -185,6 +238,58 @@ class SessionService:
             results.append(self._calculate_distance_pixels(expressions_coords[0], expressions_coords[1]))
 
         return results
+
+    def _calculate_distance_from_expression_pts(self, results_by_expression, expressions, left_pts, right_pts):
+        if len(expressions) != 1:
+            raise ValueError("Deve haver exatamente uma expressão para comparar.")
+
+        results = []
+        filtered_items = [item for item in results_by_expression if any(key in expressions for key in item)]
+
+        for side in ['left', 'right']:
+            for item in filtered_items:
+                for expression, data in item.items():
+                    expression_data = self.get_px_pts_from_detection_result(
+                                left_pts if side == 'left' else right_pts,
+                                mp.Image.create_from_file(data.get('file_path')),
+                                data.get('result')
+                            )
+                    distance = self._calculate_distance_pixels(next(iter(expression_data[0].values())), next(iter(expression_data[1].values())))
+                    results.append(distance)
+
+        return results
+
+    def _calculate_distance_mouth_variation(self, results_by_expression, expressions, left_pt, right_pt, reference_pt):
+        if len(expressions) != 1:
+            raise ValueError("Deve haver exatamente uma expressão para comparar.")
+
+        filtered_items = [
+            item for item in results_by_expression
+            if any(key in expressions for key in item)
+        ]
+
+        def get_point_coordinates(point, item):
+            expression_data = next(iter(self.get_px_pts_from_detection_result(
+                [point],
+                mp.Image.create_from_file(item['file_path']),
+                item['result']
+            )[0].values()))
+            return expression_data
+
+        points = [
+            get_point_coordinates(pt, list(item.values())[0])
+            for pt in [left_pt, right_pt, reference_pt]
+            for item in filtered_items
+        ]
+
+        paralyzed_pt = points[0 if self.paralyzed_side == 'left' else 1]
+        normal_pt = points[1 if self.paralyzed_side == 'left' else 0]
+        ref_pt = points[2]
+
+        normal_distance = self._calculate_distance_pixels(normal_pt, ref_pt)
+        paralyzed_distance = self._calculate_distance_pixels(paralyzed_pt, ref_pt)
+
+        return [normal_distance, paralyzed_distance]
 
     def _calculate_higher_variation_point(self, results_by_expression, expressions, left_pts, right_pts):
         if len(expressions) != 2:
