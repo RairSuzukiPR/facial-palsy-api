cce07325996831893d47ac217975c41a30db2ef9
Rair Suzuki <rairsuzukipr@gmail.com>
2024-12-31 14:05:04 -0300

    fix token expiration time

--- app/core/security.py
+++ app/core/security.py
@@ -20,7 +20,7 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
