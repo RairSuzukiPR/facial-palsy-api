bbe68d7c277ce8010ff8ba12f72ac2c410d900e5
Rair Suzuki <rairsuzukipr@gmail.com>
2025-02-08 19:11:31 -0300

    update get sessions to new result format

--- app/api/sessions.py
+++ app/api/sessions.py
@@ -38,19 +38,30 @@ async def process_session(data: ProcessSessionPayload, db: mysql.connector.MySQL
 async def get_users(db: mysql.connector.MySQLConnection = Depends(get_db_connection), user: dict = Depends(verify_token)):
     try:
         session_service = SessionService(db)
-        data =  session_service.get_sessions(user.get('id'))
+        data = session_service.get_sessions(user.get('id'))
 
         grouped = defaultdict(lambda: {
             'session_id': None,
             'house_brackmann': None,
             'sunnybrook': None,
-            'eyes_simetry': None,
-            'eyebrows_simetry': None,
-            'mouth_simetry': None,
-            'chin_simetry': None,
+            'hb_eyes_simetry': None,
+            'hb_mouth_simetry': None,
+            'sb_forehead_wrinkle_simetry': None,
+            'sb_gentle_eye_closure_simetry': None,
+            'sb_smile_simetry': None,
+            'sb_snarl_simetry': None,
+            'sb_lip_pucker_simetry': None,
             'eyes_synkinesis': None,
             'eyebrows_synkinesis': None,
             'mouth_synkinesis': None,
+            'mouth_synkinesis_by_raising_eyebrows': None,
+            'eyebrows_synkinesis_by_closing_eyes': None,
+            'mouth_synkinesis_by_closing_eyes': None,
+            'eyebrows_synkinesis_by_smiling': None,
+            'eyes_synkinesis_by_smiling': None,
+            'eyes_synkinesis_by_snarl': None,
+            'eyebrows_synkinesis_by_lip_pucker': None,
+            'eyes_synkinesis_by_lip_pucker': None,
             'processed_at': None,
             'photos': []
         })
@@ -60,13 +71,24 @@ async def get_users(db: mysql.connector.MySQLConnection = Depends(get_db_connect
             grouped[session_id]['session_id'] = item['session_id']
             grouped[session_id]['house_brackmann'] = item['house_brackmann']
             grouped[session_id]['sunnybrook'] = item['sunnybrook']
-            grouped[session_id]['eyes_simetry'] = item['eyes_simetry']
-            grouped[session_id]['eyebrows_simetry'] = item['eyebrows_simetry']
-            grouped[session_id]['mouth_simetry'] = item['mouth_simetry']
-            grouped[session_id]['chin_simetry'] = item['chin_simetry']
+            grouped[session_id]['hb_eyes_simetry'] = item['hb_eyes_simetry']
+            grouped[session_id]['hb_mouth_simetry'] = item['hb_mouth_simetry']
+            grouped[session_id]['sb_forehead_wrinkle_simetry'] = item['sb_forehead_wrinkle_simetry']
+            grouped[session_id]['sb_gentle_eye_closure_simetry'] = item['sb_gentle_eye_closure_simetry']
+            grouped[session_id]['sb_smile_simetry'] = item['sb_smile_simetry']
+            grouped[session_id]['sb_snarl_simetry'] = item['sb_snarl_simetry']
+            grouped[session_id]['sb_lip_pucker_simetry'] = item['sb_lip_pucker_simetry']
             grouped[session_id]['eyes_synkinesis'] = item['eyes_synkinesis']
             grouped[session_id]['eyebrows_synkinesis'] = item['eyebrows_synkinesis']
             grouped[session_id]['mouth_synkinesis'] = item['mouth_synkinesis']
+            grouped[session_id]['mouth_synkinesis_by_raising_eyebrows'] = item['mouth_synkinesis_by_raising_eyebrows']
+            grouped[session_id]['eyebrows_synkinesis_by_closing_eyes'] = item['eyebrows_synkinesis_by_closing_eyes']
+            grouped[session_id]['mouth_synkinesis_by_closing_eyes'] = item['mouth_synkinesis_by_closing_eyes']
+            grouped[session_id]['eyebrows_synkinesis_by_smiling'] = item['eyebrows_synkinesis_by_smiling']
+            grouped[session_id]['eyes_synkinesis_by_smiling'] = item['eyes_synkinesis_by_smiling']
+            grouped[session_id]['eyes_synkinesis_by_snarl'] = item['eyes_synkinesis_by_snarl']
+            grouped[session_id]['eyebrows_synkinesis_by_lip_pucker'] = item['eyebrows_synkinesis_by_lip_pucker']
+            grouped[session_id]['eyes_synkinesis_by_lip_pucker'] = item['eyes_synkinesis_by_lip_pucker']
             grouped[session_id]['processed_at'] = item['processed_at']
             # grouped[session_id]['photos'].append(item['photo_id'])
             # photo_path = os.path.join(os.getcwd(), f"app/assets/{item['photo_id']}.jpg")
